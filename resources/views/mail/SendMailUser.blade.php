63e710cde091f343297e4b6688e76067f989e7b8
andreneves <andr@andr.com.br>
2023-03-31 20:07:53 -0300

    Initial commit

---
+++ resources/views/mail/SendMailUser.blade.php
@@ -0,0 +1,10 @@
+<x-mail::message>
+# Introduction<br>
+
+{{ $email->subject }}<br>
+{{ $email->content }}
+
+
+Thanks,<br>
+{{ config('app.name') }}
+</x-mail::message>
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MailController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+ 
+Route::get('/mail/create', [MailController::class, 'create'])->name('mail.create');
+
+Route::post('/mail/create', [MailController::class, 'store'])->name('mail.store');
+
